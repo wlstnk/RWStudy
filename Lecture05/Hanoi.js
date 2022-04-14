52073e1a68ac21d7691eb07e182959b17be2aa97
Dante <wlstnk@naver.com>
2022-04-14 18:15:40 +0900

    [PJS] 출력,이동까진 만들었으나 정렬 수정 필요

--- Lecture05/Hanoi.js
+++ Lecture05/Hanoi.js
@@ -1,12 +1,80 @@
 
-var bclickOnce=false;
-function OnClickBtn_Init()
+var firstArr = [];
+var secondArr = [];
+var thirdArr = [];
+
+var save;
+var savePosition;
+var bisSaved = false;
+var bisInit = false;
+function OnClickFirst(Arr,Position)
 {
-    console.log("InitBtnClick");
-    const element = document.querySelector(".first");
-    if(bclickOnce==false)
+    console.log(savePosition,Position);
+    if(bisSaved)
     {
-        element.innerHTML+='<p>🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥</p><p>🟨🟨🟨🟨🟨🟨🟨🟨🟨</p><p>🟩🟩🟩🟩🟩🟩🟩</p><p>🟦🟦🟦🟦🟦</p><p>🟪🟪🟪</p><p>⬛</p>';
-        bclickOnce=true;
+        Arr.push(save);
+        DrawScreen();
+        bisSaved = false;
     }
+    else if(bisInit==false)
+    {
+        alert("초기화를 하지 않았습니다.");
+    }
+    else
+    {
+        savePosition = Position;
+        bisSaved=true;
+        save=Arr.pop();
+        DrawScreen();
+    }
+}
+ var bclickOnce = false;
+ function Init()
+{
+    bisInit = true;
+    save = null;
+    savePosition = null;
+    bisSaved = false;
+    
+    firstArr = [];
+    secondArr = [];
+    thirdArr = [];
+    firstArr.push("<p>🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥</p>");
+    firstArr.push("<p>🟨🟨🟨🟨🟨🟨🟨🟨🟨</p>");
+    firstArr.push("<p>🟩🟩🟩🟩🟩🟩🟩</p>");
+    firstArr.push("<p>🟦🟦🟦🟦🟦</p>");
+    firstArr.push("<p>🟪🟪🟪</p>");
+    firstArr.push("<p>⬛</p>");
+
+    const element = document.querySelector(".first");
+    DrawScreen();
+ }
+
+ function DrawScreen()
+ {
+    const firstElement = document.querySelector(".first");
+    const secondElement = document.querySelector(".second");
+    const thirdElement = document.querySelector(".third");
+
+    // firstElement.innerHTML="first";
+    // firstArr.forEach(function(i){
+    //     firstElement.innerHTML += i;
+    // });
+    
+    firstArr.sort((x,y)=>x.length-y.length);
+    secondElement.innerHTML="first";
+    secondArr.forEach(function(i){
+        secondElement.innerHTML += i;
+    });
+
+
+    secondElement.innerHTML="second";
+    secondArr.forEach(function(i){
+        secondElement.innerHTML += i;
+    });
+
+    thirdElement.innerHTML="second";
+    thirdArr.forEach(function(i){
+         thirdElement.innerHTML += i;
+    });
  }
